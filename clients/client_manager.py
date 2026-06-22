9e4623fc4440d583e3a2a8b64598370645a387f4
Ju5tADeve10per <t0aruka1hat5u5ya@gmail.com>
2026-06-23 02:05:33 +0900

    feat: implement client based on redesigned architecture

--- clients/client_manager.py
+++ clients/client_manager.py
@@ -1,58 +1,47 @@
-from request_builder import make_post_request
-from send_signal import send_post_request
+from send_signal import send_post_request, display_response_status
 import time
 
 URL = "http://localhost:8000/signal"
 
-# current_clients = [
+# current_clients = {}
 #   "client1",
 #   "client2"
-#]
-current_clients = [] # TODO: データ構造上重複するけどどうする？
+#}
+current_clients = set()
+
+class ClientManager:
+    # TODO: create_client, _add_client, show_client_listをここにいれ、ほかに必要な関数もここに書く。
 
 def create_client(client_id: str): # この関数はクライアントを登録する。それはサーバ側にも最初の信号を送ることを含む。
-    # input client info -> id
-    get_current_clients().append(client_id) # これだと
+    add_client(client_id)
     data = make_post_request(client_id)
     result = send_post_request(URL, data)
     return result
 
-def send_client_heartbeat(client_id):
-    data = make_post_request(client_id)
-    result = send_post_request(URL, data)
-    return result
-
-def get_current_clients():
-    return current_clients
+def _add_client(client_id: str) -> bool: # boolの結果でfalseなら失敗、そうでなければ成功
+    if client_id in current_clients:
+        return false:
+    current_clients.add(client_id)
+    return true
 
 def show_client_list():
-    client_list = get_current_clients()
-    client_no = 0
-    for client_id in client_list:
+    # TODO: current_clientsの中身が空ならno client registeredみたいなのprint
+    for client_no, client_id in enumerate(sorted(current_clients), start=1):
         print(f"\033[32mNo.{client_no}\033[0m: \033[31m{client_id}\033[0m")
-        client_no += 1
 
-def make_post_request(client_id: str) -> dict:
-    data = {
-        "id": client_id,
-        "timestamp": int(time.time())
-    }
-    return data
-
-# client_manager.py
-Client {
-    1. send_client_heartbeat
-    2. make_post_request
-    3. クライアント自身の情報。idと最後のタイムスタンプを持ち管理する関数
-}
-
-ClientManager {
-    create_client: 新規クライアントを登録するため
-    add_client: create_clientから呼ばれる。グローバル変数であるcurrent_clientsに何かを追加する場合は必ず、この関数を通す
-    show_client_list
-    delete_client: グローバル変数であるcurrent_clientsから何かを削除する場合は必ず、この関数を通す
-}
-
-# send_signal.py
-send_post_request
-display_response_status
+ class Client:
+    def __init__: # <- ここが最初に呼ばれるのは、インスタンスが生成されるcreate_client内
+        self.id # <-これはcontrol_room.pyでのinputをここに割り当てたい
+        self.timestamp # <- これはcreate_client内で最初のインスタンスが作られるときに割り当てられる
+    
+    def send_client_heartbeat(self.id):
+        payload = _build_post_request(self.id)
+        result = send_post_request(URL, payload)
+        return result # この結果は必ずdisplay_response_statusに渡し結果を出力することが前提
+    
+    def _build_post_request(self.id):
+        payload = {
+            "id": client_id,
+            "timestamp": int(time.time())
+        }
+        return payload
