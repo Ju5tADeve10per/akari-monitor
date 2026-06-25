a5611f3fe71c61c4457bdb6552a3197f7ba48ac4
Ju5tADeve10per <t0aruka1hat5u5ya@gmail.com>
2026-06-26 02:33:47 +0900

    feat: implement control_room.py functions (WIP)

--- clients/control_room.py
+++ clients/control_room.py
@@ -4,20 +4,23 @@
 # 3. 全クライアントを確認する。
 # 4. 特定のクライアントのデータを消す。(これはあとでやる。サーバ側でも書く必要があるため)
 
-from client_manager import create_client, show_client_list
+from client_manager import ClientManager, Client
 from send_signal import display_response_status
 
-def main():
+def control_tower(manager):
     while (1):
         print("What do you wanna do?")
         print("1. Make a new client\n2. Send a new signal from existed client\n3. Check all clients\n4. Nah, I'm good.")
         res = int(input()) # TODO: 数字以外でクラッシュするのでどうする？
         if res == 1:
             # input client id
-            client_id = input()
-            res = create_client(client_id)
-            # show the result
-            display_response_status(res)
+            client_id = input() # return str TODO: need type check?
+            new_client = Client(client_id)
+            res = manager.create_client(new_client)
+            if res:
+                print("register successful.")
+            else:
+                print("register failed. Check if the client is already existed.")
         elif res == 2:
             # クライアントリストからクライアントを選ぶ
             # send_client_heartbeatを使うことでシグナルをサーバに送る。
@@ -30,3 +33,7 @@ def main():
             exit() # python的にはこれでループ抜ける？
         else:
             print("Invalid Input. Try again")
+
+def __name__ == __main__:
+manager = ClientManager()
+control_tower(manager)
