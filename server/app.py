c1b03e7622fee5558c10dff9e167e4fb40f5af42
Ju5tADeve10per <t0aruka1hat5u5ya@gmail.com>
2026-08-06 00:32:03 +0900

    fix: add CORS support to server

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from routes import bp
 
 # Create the Flask application instance
 app = Flask(__name__)
+CORS(app)
 
 # Register the blueprint (routes)
 app.register_blueprint(bp)
